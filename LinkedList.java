e8e67e6ad919b3e91aa120b922cf182959b38eb0
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-09-05 20:07:50 -0400

    Create initial .java file with class and main

---
+++ LinkedList.java
@@ -0,0 +1,27 @@
+/* 
+  Author: Alessio Mazzone
+  
+  Implementation of LinkedList data structure
+  and some associated methods, plus a main to
+  test the class.
+*/
+
+public class LinkedList
+{
+  /* Private inner class
+    Nodes are the structures linked by
+    the linked list. 
+  */
+  private class Node
+  {
+
+  }  
+
+  // Implement: Insert, Delete, Search, Print
+
+
+  public static void main(String args[])
+  { 
+    System.out.println("Hi.");
+  }
+}
