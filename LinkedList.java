9995491324bcbefb6a6f760d79b262386ce96368
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-09-07 14:09:20 -0400

    Implemented add() funcitonality.

--- LinkedList.java
+++ LinkedList.java
@@ -13,11 +13,47 @@ public class LinkedList<T>
 
   public LinkedList()
   {
-
+    head = null;
+    numberOfData = 0;
   }
 
+  // Implement: Add, Remove, Search, toArray, Print
 
-  // Implement: Add, Delete, Search, Print
+  /* 
+  This method takes one argument: the data user wishes to add.
+  This method returns one argument: true/false based on if add was sucessful. 
+  
+  This method will create a new Node and make it the new head of the list,
+  pushing all other Nodes down. 
+  */
+  public boolean add(T newData)
+  {
+    try
+    {
+      Node newNode = new Node(newData);
+
+      if(numberOfData == 0)
+      {
+        head = newNode;
+        numberOfData++;
+        
+        return true;
+      }
+      
+      newNode.next = head;
+      head = newNode;
+        
+      return true;
+    }
+    catch(Exception e)
+    {
+      System.out.println("Item could not be added to linked list due to an error.");
+      return false;
+    }
+  }
+
+  
+
 
 
 
@@ -42,16 +78,17 @@ public class LinkedList<T>
       this(nodeData, null);
     }
 
-    // Assigns data 
+    // Assigns data and next node 
     private Node(T nodeData, Node nextNode)
     {
       data = nodeData;
       next = nextNode;
     }
 
-    /* No setter and getter methods are necessary.
-       LinkedList, being the outerclass of Node, will
-       have access to Node's data directly.
+    /* 
+      No setter and getter methods are necessary.
+      LinkedList, being the outerclass of Node, will
+      have access to Node's data directly.
     */ 
 
   }
@@ -60,6 +97,24 @@ public class LinkedList<T>
   // A main mehtod to test the functionality of the LinkedList.
   public static void main(String args[])
   { 
-    System.out.println("Hi.");
+    // Create a linked list
+    LinkedList<String> aList = new LinkedList();
+
+    // Test add() method. Will print true or false based on if data was added or not.
+    String item1 = "Hello";
+    String item2 = "World";
+    String item3 = "Dount";
+
+    boolean item1added = aList.add(item1);
+    boolean item2added = aList.add(item2);
+    boolean item3added = aList.add(item3);
+
+    System.out.println("Item 1: " + item1); 
+    System.out.println("Sucessfuly added to list?: " + item1added);
+    System.out.println("Item 2: " + item2); 
+    System.out.println("Sucessfuly added to list?: " + item2added);
+    System.out.println("Item 3: " + item3); 
+    System.out.println("Sucessfuly added to list?: " + item3added);
+
   }
 }
