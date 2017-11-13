a6a095f43c1b57d41100e4124cbc0a3b84b16d74
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-11-13 15:46:21 -0500

    Algorithm doesn't work. Downloading program so I can trace it.

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,4 +1,4 @@
-/* 
+/*
   Author: Alessio Mazzone
   
   Implementation of Insertion sort
@@ -12,13 +12,42 @@
     This algorithm takes an item from the unsorted part of the array
     and places it in the correct spot in the sorted portion of the array.
  */
-public class InsertionSort<T>
+
+import java.util.*;
+
+public class InsertionSort
 {
     
-    
-    public T[] insertionSort(T[] array)
+    public static int[] insertionSort(int[] array)
     {
-        return null;
+        int data;
+        
+        for(int unsortedIndex = 1; unsortedIndex < array.length; unsortedIndex++)
+        {
+            // Grab the data
+            data = array[unsortedIndex];
+            
+            
+            for(int sortedIndex = unsortedIndex - 1; sortedIndex < 0; sortedIndex--)
+            {
+                    // Compare
+                    if(array[sortedIndex] > data)
+                    {
+                        // Swap data
+                        array[sortedIndex + 1] = array[sortedIndex];
+                        array[sortedIndex] = data;
+                        
+                    }
+                    else // array[sortedIndex] <= array[unsortedIndex]
+                    {
+                        // Item is in correct spot, so break out of loop
+                        sortedIndex = -1;
+                    }
+            }
+        }
+        
+        
+        return array;
     }
 
 
@@ -27,7 +56,17 @@ public class InsertionSort<T>
 
     public static void main(String[] args)
     {
-        System.out.println("Piacere!");
+        
+        int[] array = {3,2,6,1,5};
+        
+        // Print unsorted array
+        System.out.println("UNSORTED ARRAY:");
+        System.out.println(Arrays.toString(array) + "\n");
+        
+        array = insertionSort(array);
+        
+        System.out.println("SORTED ARRAY:");
+        System.out.println(Arrays.toString(array) + "\n");
     }
 }
 
