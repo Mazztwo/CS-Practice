53965569905de5e5fdaccba257061f69b1f54a3b
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-12-10 22:00:46 -0500

    Made new Eclipse directory to hold solutions.

---
+++ CodeFights/Interview_Practice/Arrays/src/Q1_FirstDuplicate.java
@@ -0,0 +1,16 @@
+public class Q1_FirstDuplicate 
+{
+
+	int firstDuplicate(int[] a)
+	{
+		
+		
+		return 0;
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("hi!");
+	}
+}
